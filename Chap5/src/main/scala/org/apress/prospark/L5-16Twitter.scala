21fd3bc4f215a3281ce0e711028a486936c24273
Zubair Nabi <zubair@qubit.com>
2016-03-27 11:55:21 +0500

    Twitter example added to Chap5.

---
+++ Chap5/src/main/scala/org/apress/prospark/L5-16Twitter.scala
@@ -0,0 +1,49 @@
+package org.apress.prospark
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD.rddToOrderedRDDFunctions
+import org.apache.spark.streaming.Seconds
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.dstream.DStream.toPairDStreamFunctions
+import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.spark.storage.StorageLevel
+import twitter4j.conf.ConfigurationBuilder
+import twitter4j.TwitterFactory
+
+object TwitterApp {
+
+  def main(args: Array[String]) {
+    if (args.length != 3) {
+      System.err.println(
+        "Usage: TwitterApp <master> <appname> <outputPath>" +
+          " In local mode, <master> should be 'local[n]' with n > 1")
+      System.exit(1)
+    }
+
+    val Seq(master, appName, outputPath) = args.toSeq
+
+    val conf = new SparkConf()
+      .setAppName(appName)
+      .setMaster(master)
+      .setJars(SparkContext.jarOfClass(this.getClass).toSeq)
+
+    val ssc = new StreamingContext(conf, Seconds(10))
+
+    val cb = new ConfigurationBuilder()
+    cb.setOAuthConsumerKey("")
+    cb.setOAuthConsumerSecret("")
+    cb.setOAuthAccessToken("")
+    cb.setOAuthAccessTokenSecret("")
+
+    val twitterAuth = new TwitterFactory(cb.build()).getInstance().getAuthorization()
+
+    val tweetStream = TwitterUtils.createStream(ssc, Some(twitterAuth), Array("nyc citi bike", "nyc bike share"))
+    tweetStream.count().print()
+    tweetStream.saveAsTextFiles(outputPath)
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+}
